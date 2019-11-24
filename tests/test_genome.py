9a9af6568a7b1842b0e68a39b2236e2845a1a6a7
Adam Tupper <adam.tupper@outlook.com>
2019-11-24 18:15:08 +1300

    Add test for ConnectionGene constructor.

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -3,7 +3,7 @@ Tests for the genome encoding module.
 """
 
 import pytest
-from peal.genome import NodeTypes, NodeGene
+from peal.genome import NodeTypes, NodeGene, ConnectionGene
 
 __author__ = "Adam Tupper"
 __copyright__ = "Adam Tupper"
@@ -16,3 +16,23 @@ def test_create_node_gene():
     """
     node_gene = NodeGene(type=NodeTypes.INPUT)
     assert node_gene.type == NodeTypes.INPUT
+
+def test_create_connection_gene():
+    """
+    Test the creation of connection genes.
+    """
+    in_node = NodeGene(NodeTypes.INPUT)
+    out_node = NodeGene(NodeTypes.HIDDEN)
+    connection_gene = ConnectionGene(
+        in_node=in_node,
+        out_node=out_node,
+        weight=1.0,
+        expressed=True,
+        innov_num=0
+    )
+
+    assert connection_gene.in_node == in_node
+    assert connection_gene.out_node == out_node
+    assert connection_gene.weight == pytest.approx(1.0)
+    assert connection_gene.expressed == True
+    assert connection_gene.innov_num == 0
